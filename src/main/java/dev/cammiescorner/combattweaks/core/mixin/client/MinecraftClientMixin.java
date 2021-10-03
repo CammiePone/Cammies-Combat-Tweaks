aa2b32c28f9994258db7b8319815630ca7264b9a
CammiePone <inazuma261@gmail.com>
2021-10-03 17:38:23 -0400

    added attack queuing

--- src/main/java/dev/cammiescorner/combattweaks/core/mixin/client/MinecraftClientMixin.java
+++ src/main/java/dev/cammiescorner/combattweaks/core/mixin/client/MinecraftClientMixin.java
@@ -3,31 +3,52 @@ package dev.cammiescorner.combattweaks.core.mixin.client;
 import dev.cammiescorner.combattweaks.client.CombatTweaksClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
 	@Shadow @Nullable public ClientPlayerEntity player;
 	@Shadow protected int attackCooldown;
+
+	@Unique private boolean attackQueued = false;
+
+	@Redirect(method = "handleInputEvents", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/client/option/KeyBinding;wasPressed()Z",
+			ordinal = 13
+	))
+	public boolean queueAttack(KeyBinding instance) {
+		return instance.wasPressed() || attackQueued;
+	}
 
 	@Inject(method = "handleInputEvents", at = @At(value = "INVOKE",
 			target = "Lnet/minecraft/client/MinecraftClient;doAttack()V",
 			ordinal = 0
 	), cancellable = true)
-	public void onLeftClick(CallbackInfo info) {
-		if(player != null && (player.getAttackCooldownProgress(0.5F) < 1F || player.getItemCooldownManager().isCoolingDown(player.getMainHandStack().getItem())))
-			info.cancel();
+	public void handleAttacking(CallbackInfo info) {
+		if(player != null) {
+			if(player.getAttackCooldownProgress(0.5F) > 0.33F)
+				attackQueued = true;
+
+			if(player.getAttackCooldownProgress(0.5F) < 1F || player.getItemCooldownManager().isCoolingDown(player.getMainHandStack().getItem()))
+				info.cancel();
+		}
+
+		if(!info.isCancelled() && attackQueued)
+			attackQueued = false;
 	}
 
 	@Inject(method = "doAttack", at = @At(value = "INVOKE",
 			target = "Lnet/minecraft/client/network/ClientPlayerEntity;resetLastAttackedTicks()V"
 	))
-	public void doAttack(CallbackInfo info) {
+	public void removeAttackCooldown(CallbackInfo info) {
 		if(CombatTweaksClient.isEnabled)
 			attackCooldown = 0;
 	}
